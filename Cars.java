5c6ff0d86156e535f09e4d04c3a5b6e4c49f67ed
letoanban <letoanban.word@gamil.com>
2024-02-29 08:46:40 +0700

    Up files Tuan02

---
+++ Cars.java
@@ -0,0 +1,17 @@
+package myPackage;
+
+public class Cars {
+	int id;
+	String name;
+	
+	public Cars(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public static void main(String[] args) {
+		Cars car1 = new Cars(1, "BMW");
+		Cars car2 = new Cars(2, "Ducati");
+		Cars car3 = new Cars(3, "Honda");
+	}
+}
